d9ca6065189c4aa8c5344ec673ce67fff89f7c86
Victor Widell <victor.widell@gmail.com>
2025-01-25 02:25:48 +0100

    Test: getCategorizedReactNode class

--- src/categorized-react-node.ts
+++ src/categorized-react-node.ts
@@ -1,15 +1,24 @@
-import { ReactElement, ReactHTMLElement, ReactNode } from "react";
+import { Component, ReactElement, ReactHTMLElement, ReactNode } from "react";
 
 type FunctionReactElement = ReactElement<
     object,
     (props: object, deprecatedLegacyContext?: unknown) => ReactNode
 >;
 
+type ClassReactElement = ReactElement<
+    object,
+    new (props: object, deprecatedLegacyContext?: unknown) => Component<
+        object,
+        unknown
+    >
+>;
+
 export type CategorizedReactNode =
     | { type: "simple"; node: string | number | boolean | null | undefined }
     | { type: "iterable"; node: Iterable<ReactNode> }
     | { type: "html"; node: ReactHTMLElement<HTMLElement> }
-    | { type: "function"; node: FunctionReactElement };
+    | { type: "function"; node: FunctionReactElement }
+    | { type: "class"; node: ClassReactElement };
 
 export function getCategorizedReactNode(node: ReactNode): CategorizedReactNode {
     if (node === null || typeof node !== "object") {
@@ -25,10 +34,15 @@ export function getCategorizedReactNode(node: ReactNode): CategorizedReactNode {
     }
 
     if (typeof node.type === "function") {
-        return {
-            type: "function",
-            node: node as FunctionReactElement,
-        };
+        return Object.getPrototypeOf(node.type) !== Component
+            ? {
+                  type: "function",
+                  node: node as FunctionReactElement,
+              }
+            : {
+                  type: "class",
+                  node: node as ClassReactElement,
+              };
     }
 
     console.log(node);
