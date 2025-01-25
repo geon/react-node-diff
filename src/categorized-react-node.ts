b08e114a082eea3d467f6f4d7ffd2d3d48e59b12
Victor Widell <victor.widell@gmail.com>
2025-01-25 01:58:00 +0100

    Test: getCategorizedReactNode html

--- src/categorized-react-node.ts
+++ src/categorized-react-node.ts
@@ -1,8 +1,9 @@
-import type { ReactNode } from "react";
+import type { ReactHTMLElement, ReactNode } from "react";
 
 export type CategorizedReactNode =
     | { type: "simple"; node: string | number | boolean | null | undefined }
-    | { type: "iterable"; node: Iterable<ReactNode> };
+    | { type: "iterable"; node: Iterable<ReactNode> }
+    | { type: "html"; node: ReactHTMLElement<HTMLElement> };
 
 export function getCategorizedReactNode(node: ReactNode): CategorizedReactNode {
     if (node === null || typeof node !== "object") {
@@ -11,6 +12,10 @@ export function getCategorizedReactNode(node: ReactNode): CategorizedReactNode {
 
     if (Symbol.iterator in node) {
         return { type: "iterable", node };
+    }
+
+    if (typeof node.type === "string") {
+        return { type: "html", node: node as ReactHTMLElement<HTMLElement> };
     }
 
     throw new Error("Unknown ReactNode category.", { cause: node });
