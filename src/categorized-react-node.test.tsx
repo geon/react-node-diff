63bfa716939507647f6b0bebcab1d507acf62403
Victor Widell <victor.widell@gmail.com>
2025-01-25 01:46:23 +0100

    Test: getCategorizedReactNode iterable

--- src/categorized-react-node.test.tsx
+++ src/categorized-react-node.test.tsx
@@ -8,3 +8,7 @@ test("getCategorizedReactNode simple", () => {
     expect(getCategorizedReactNode(null)).toHaveProperty("type", "simple");
     expect(getCategorizedReactNode(undefined)).toHaveProperty("type", "simple");
 });
+
+test("getCategorizedReactNode iterable", () => {
+    expect(getCategorizedReactNode([1, 2])).toHaveProperty("type", "iterable");
+});
--- src/categorized-react-node.ts
+++ src/categorized-react-node.ts
@@ -1,13 +1,16 @@
 import type { ReactNode } from "react";
 
-export type CategorizedReactNode = {
-    type: "simple";
-    node: string | number | boolean | null | undefined;
-};
+export type CategorizedReactNode =
+    | { type: "simple"; node: string | number | boolean | null | undefined }
+    | { type: "iterable"; node: Iterable<ReactNode> };
 
 export function getCategorizedReactNode(node: ReactNode): CategorizedReactNode {
     if (node === null || typeof node !== "object") {
         return { type: "simple", node };
+    }
+
+    if (Symbol.iterator in node) {
+        return { type: "iterable", node };
     }
 
     throw new Error("Unknown ReactNode category.", { cause: node });
